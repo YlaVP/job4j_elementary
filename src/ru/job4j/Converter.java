fcac076f36dc158dd9b05067ea21e6c28469aa38
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-09-05 11:13:56 +0900

    Исправление ошибки в задаче Converter

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -1,24 +1,24 @@
 package ru.job4j;
 
 public class Converter {
-    public static int rubleToEuro(int value) {
-        int rsl = value * 70;
-        return rsl;
+    public static int rubleToEuro(int value1) {
+        int rsl1 = value1 / 70;
+        return rsl1;
 
     }
 
-    public static int rubleToDollar(int value) {
-        int rsl = value * 60;
-        return rsl;
+    public static int rubleToDollar(int value2) {
+        int rsl2 = value2 / 60;
+        return rsl2;
 
     }
 
     public static void main(String[] args) {
         double euro = 1.0 / 70.0;
-        int rsl = 70;
+        int value1 = 1;
 
         double dol = 1.0 / 60.0;
-        int rsl2 = 60;
+        int value2 = 1;
         System.out.println(" euro = " + euro);
         System.out.println("dol = " + dol);
 
