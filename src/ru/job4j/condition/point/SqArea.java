7233b47761808cc98f124fff86192e4545c8807e
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-09-08 23:28:30 +0900

    8. Стороны прямоугольника. [#341207]

--- src/ru/job4j/condition/point/SqArea.java
+++ src/ru/job4j/condition/point/SqArea.java
@@ -1,19 +1,19 @@
 package ru.job4j.condition.point;
 
-//public class SqArea {
+public class SqArea {
 
-   // public static double square(int p, int k) {
-     //   double p = (L + h)*2;
+    public static double square(int p, int k) {
+        double h = p / 2 * (k + 1);
+        double l = h * k;
+        double rsl = l * h;
+        return rsl;
+    }
 
-      //  double rsl = L * h;
-     //   return rsl;
-   // }
-
-  //  public static void main(String[] args) {
-      //  double result1 = SqArea.square(6, 2);
-   //     System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
-    //}
- //}
+    public static void main(String[] args) {
+        double result1 = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+    }
+ }
 
 
 
