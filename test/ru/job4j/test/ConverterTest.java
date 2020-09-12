14d0acef14ee30e8ca48398829ee0ab3749f3b13
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-09-12 20:17:19 +0900

    Модульные тесты. [#341243]

---
+++ test/ru/job4j/test/ConverterTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = '0';
+        int expected = '2';
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+
+        }
+
+        @Test
+
+        public void whenConvert0RblThen3Dlr() {
+            int in1 = 0;
+            int expected2 = 3;
+            int out3 = Converter.rubleToDollar(in1);
+            Assert.assertEquals(expected2, out3);
+        }
+
+    }
+
+
