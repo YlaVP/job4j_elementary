61e3d0a7408e7b271eaca5bd61f478a86b43c7b4
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-09-20 15:23:59 +0900

    4. Тесты для стороны прямоугольника4. [#341246]

--- src/ru/job4j/condition2/SqArea.java
+++ src/ru/job4j/condition2/SqArea.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class SqArea {
+
     public static double square(int p, int k) {
         double h = p / 2 * (k + 1);
         double l = h * k;
@@ -15,25 +16,4 @@ public class SqArea {
         double result1 = SqArea.square(4, 1);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
     }
-
-    @Test
-
-        public void square() {
-            int expected = 162;
-            int p = 6;
-            int k = 2;
-            double out = SqArea.square(p, k);
-            Assert.assertEquals(expected, out, 0.01);
-        }
-
-        @Test
-            public void square2() {
-                int expected = 1600;
-                int p = 8;
-                int k = 4;
-                double out = SqArea.square(p, k);
-                Assert.assertEquals(expected, out, 0.01);
-            }
-    }
-
-
+}
