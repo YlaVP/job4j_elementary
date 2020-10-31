67edcf6a9534b29875da8abea7524f3a62df554b
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-10-31 21:09:58 +0900

    4.3.1. Оператор && [#341160]

---
+++ src/ru/job4j/conditon/Cinema.java
@@ -0,0 +1,21 @@
+package ru.job4j.conditon;
+
+public class Cinema {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        boolean can = allowByParent && hasMoney;
+        String label = " ";
+        if ( can ) {
+            System.out.println("I can go to the cinema.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
+    public static void main(String[] args) {
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
+    }
+}
+
