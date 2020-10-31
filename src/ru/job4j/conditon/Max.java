7f7bbaa49bff0bd29175dbad6db2f766c064b26e
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-10-31 19:41:47 +0900

    4.2. Тернарное сравнение [#341166]

--- src/ru/job4j/conditon/Max.java
+++ src/ru/job4j/conditon/Max.java
@@ -1,10 +1,13 @@
 package ru.job4j.conditon;
 
+
 public class Max {
-    public static boolean max(int left, int right) {
-        boolean result = (left > right);
+
+    public static String max(int left, int right) {
+
+        String result = left <= right ? left > right ? "left" : "right": "left max" ;
+
         return result;
     }
-
 }
 
