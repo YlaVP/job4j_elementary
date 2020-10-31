b7310a6002439e2fddd5d1e50b1033142d6086bf
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-10-31 23:09:46 +0900

    4.3.2. Оператор || [#341161]

--- src/ru/job4j/conditon/Club.java
+++ src/ru/job4j/conditon/Club.java
@@ -2,8 +2,7 @@ package ru.job4j.conditon;
 
 public class Club {
     public static void permission(boolean hasMoney, boolean beFriend) {
-        boolean can = hasMoney || beFriend;
-        if (can) {
+        if (hasMoney || beFriend) {
             System.out.println("I can go to the club.");
         } else {
             System.out.println("I can't.");
