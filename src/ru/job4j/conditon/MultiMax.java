634ba0778716c9008ca55af912e68376af7fb9ff
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-11-03 19:54:14 +0900

    4.4. Вложенный оператор if else [#341167]

--- src/ru/job4j/conditon/MultiMax.java
+++ src/ru/job4j/conditon/MultiMax.java
@@ -3,10 +3,11 @@ package ru.job4j.conditon;
 public class MultiMax {
     public static int max(int first, int second, int third) {
 
-    int temp = first > second && first > third ? first : third;
-    int temp1 = second > third ? second : third;
-    int rsl = temp > temp1 ? temp : temp1;
-    return  rsl;
+        int temp = first > second ? first : second;
+
+        int rsl = temp > third ? temp : third;
+
+        return rsl;
     }
 
 }
