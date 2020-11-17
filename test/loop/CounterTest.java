5589d4fa781a6b19c17aa249d7fcb55b24360ebc
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-11-17 22:32:22 +0900

    5.1. Подсчет суммы чётных чисел в диапазоне [#341267]

---
+++ test/loop/CounterTest.java
@@ -0,0 +1,24 @@
+package loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumByEvenNumbersFromOneToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat(rsl, is(expected));
+
+    }
+
+    @Test
+    public void whenSumByEvenNumberFromThreeToEight() {
+        int rsl = Counter.sumByEven(3,8);
+        int expected = 18;
+        assertThat(rsl, is(expected));
+    }
+}
