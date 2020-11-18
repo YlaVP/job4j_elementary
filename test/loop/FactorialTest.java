85bacf63a202451c74fb35b60254a0efb8d7bdd6
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-11-18 23:10:03 +0900

    5.2. Создать программу, вычисляющую факториал. [#341268]

---
+++ test/loop/FactorialTest.java
@@ -0,0 +1,27 @@
+package loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class FactorialTest {
+
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+    int result = Factorial.calc(5);
+    int expected = 120;
+    assertThat(result, is(expected));
+
+
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        int result = Factorial.calc(0);
+        int expected = 1;
+        assertThat(result, is(expected));
+
+    }
+}
