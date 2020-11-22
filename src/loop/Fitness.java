bff017c62cfbb46febfab8ac81fbbc298c9c752d
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-11-22 22:18:26 +0900

    5.4. Протеиновая диета [#341259]

---
+++ src/loop/Fitness.java
@@ -0,0 +1,23 @@
+package loop;
+
+
+import java.time.Month;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        var powerIvan = ivan;
+        var powerNik = nik;
+        while (powerIvan < powerNik) {
+            powerIvan *= 3;
+            powerNik *= 2;
+            month++;
+        }
+            return month;
+    }
+
+}
+
+
+
+
--- test/loop/CounterTest.java
+++ test/loop/CounterTest.java
@@ -21,4 +21,6 @@ public class CounterTest {
         int expected = 18;
         assertThat(rsl, is(expected));
     }
+
+
 }
