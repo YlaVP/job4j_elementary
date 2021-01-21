8bc5b239b54cbfaafe950d6999681b81b6a89ae3
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2021-01-21 21:56:07 +0900

    6.0.3. Массивы и цикл for. [#341187]2

--- src/ru/job4j/array/ArrayLoop.java
+++ src/ru/job4j/array/ArrayLoop.java
@@ -5,8 +5,6 @@ public class ArrayLoop {
         int[] numbers = new int[5];
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index * 2 + 3;
-        }
-        for (int index = 0; index < numbers.length; index++) {
             System.out.println(numbers[index]);
         }
     }
