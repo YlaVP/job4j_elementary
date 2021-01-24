c4a68db01e9ac519469dfa1eddcfde906e4ae4ac
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2021-01-24 15:40:30 +0900

    6.0.4. Заполнить массив степенями чисел. [#341171]

---
+++ src/ru/job4j/array/Square.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+public class Square {
+
+    public static int[] calculate(int bound) {
+        int[] rst = new int[4];
+        for (int index = 0; index < rst.length; index++)
+            rst[index] = index * index;
+            /* заполнить массив через цикл элементами от 0 до bound , возведенными в квадрат */
+            return rst;
+        }
+
+
+
+        public static void main ( String[]args){
+            int[] array = calculate(4);
+            for (int index = 0; index < array.length; index++) {
+                System.out.println(array[index]);
+            }
+        }
+    }
+
+
+
