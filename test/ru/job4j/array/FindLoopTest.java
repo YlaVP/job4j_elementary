86d97319bdff1d9e55ba3d1a20a82bf35fc98177
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2021-01-27 21:58:14 +0900

    6.1. Классический поиск перебором. Исправления

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -16,12 +16,13 @@ public class FindLoopTest {
         assertThat(result, is(expect));
     }
 
-    @Test
-    public void whenArrayHas5Then1() {
+       @Test
+    public void whenArrayHas5ThenMunus1() {
         int[] input = {5, 4, 3, 2};
-        int value = 5;
+        int value = 6;
         int result = FindLoop.indexOf(input, value);
-        int expect = 1;
+        int expect = 0;
         assertThat(result, is(expect));
     }
 }
+
