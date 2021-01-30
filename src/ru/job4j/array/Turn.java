2fbb76c17772f878f11f87857507228b64dac9bb
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2021-01-30 20:27:20 +0900

    6.2.2. Переставить элементы массива. Исправления

---
+++ src/ru/job4j/array/Turn.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+        int temp;
+        int index = array.length - 1;
+
+        for (int i = 0; i <= index/2; i++ ) {
+            temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+            index--;
+        }
+
+        return array;
+    }
+}
