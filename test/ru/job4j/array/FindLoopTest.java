168e298b01da4cc5f2012fa8593060fa902fdc59
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2021-02-15 17:00:34 +0900

    6.5. Поиск индекса в диапазоне. [#341179]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -16,12 +16,24 @@ public class FindLoopTest {
         assertThat(result, is(expect));
     }
 
-       @Test
+    @Test
     public void whenArrayHas5ThenMunus1() {
         int[] input = {5, 4, 3, 2};
         int value = 6;
         int result = FindLoop.indexOf(input, value);
         int expect = 0;
+        assertThat(result, is(expect));
+
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
         assertThat(result, is(expect));
     }
 }
