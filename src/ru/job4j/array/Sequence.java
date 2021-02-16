be1088226f6b8c96510299cab442564a5ea3c105
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2021-02-16 18:34:42 +0900

    6.6.1. Двухмерный массив. Вывод на консоль. [#341192]

--- src/ru/job4j/array/Sequence.java
+++ src/ru/job4j/array/Sequence.java
@@ -4,14 +4,14 @@ public class Sequence {
     public static void main(String[] args) {
         int[][] array = new int[3][3];
         array[0][0] = 1;
-        array[0][1] = 5;
-        array[0][2] = 9;
+        array[0][1] = 4;
+        array[0][2] = 7;
         array[1][0] = 2;
-        array[1][1] = 6;
+        array[1][1] = 5;
+        array[1][2] = 8;
         array[2][0] = 3;
-        array[2][1] = 7;
-        array[3][0] = 4;
-        array[3][1] = 8;
+        array[2][1] = 6;
+        array[3][1] = 9;
 
     }
 }
