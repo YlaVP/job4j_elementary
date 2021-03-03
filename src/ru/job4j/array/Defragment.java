5727783c9a949a2e24c68d89c01bc053d7f086ce
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2021-03-03 21:30:52 +0900

    6.8. Дефрагментация массива [#341199]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -2,28 +2,24 @@ package ru.job4j.array;
 
 
 public class Defragment {
-
     public static String[] compress(String[] array) {
-        for (int index = 0; index < array.length; index++) {
-            if (array[index] == null) {
-                int point = index + 1; /* указатель на null ячейку. */
-                while (point < array.length && array[point] == null) {
-                    point++;
-                }
-                if (point < array.length) {
-                    swap(array, index, point);
+
+    for (int index = 0; index < array.length; index++) {
+        if (array[index] == null) {
+            int indexNull = index;
+            for (int findNotNull = (indexNull + 1); findNotNull < array.length; findNotNull++) {
+                if (array[findNotNull] != null) {
+                    int indexNotNull = findNotNull;
+                    String temp = array[indexNotNull];
+                    array[indexNotNull] = array[indexNull];
+                    array[indexNull] = temp;
+                    break;
                 }
             }
         }
-        return array;
     }
-
-    public static String[] swap(String[] array, int nullIndex, int notNullIndex) {
-        String temp = array[notNullIndex];
-        array[notNullIndex] = array[nullIndex];
-        array[nullIndex] = temp;
         return array;
-    }
+}
 
     public static void main(String[] args) {
         String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
