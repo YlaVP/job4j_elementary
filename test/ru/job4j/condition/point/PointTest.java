e09ddf4b234b88ebd4adde95d9ed2cbeb1585cb9
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2021-03-03 21:42:27 +0900

    6.8. Дефрагментация массива [#341199]

--- test/ru/job4j/condition/point/PointTest.java
+++ test/ru/job4j/condition/point/PointTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition.point;
 
+import junit.framework.Assert;
 import org.junit.jupiter.api.Test;
 import ru.job4j.condition.point.Point;
 
@@ -10,36 +11,38 @@ import static org.junit.Assert.*;
 public class PointTest {
     @Test
     public void when00to20then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         int expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = distance(x1, y1, x2, y2);
-        assertEquals(expected, out, 0.01);
-    }
+        double out = a.distance(b);
 
-
-
-    @Test
-    public void when11to10then1() {
-        int expected = 1;
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 1;
-        int y2 = 0;
-        double out = distance(x1, y1, x2, y2);
-        assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void when21to10then1() {
-        int expected = 1;
-        int x1 = 2;
-        int y1 = 1;
-        int x2 = 1;
-        int y2 = 1;
-        double out = distance(x1, y1, x2, y2);
-        assertEquals(expected, out, 0.01);
+    public void when12to20then2() {
+        Point a = new Point(1, 2);
+        Point b = new Point(2, 0);
+        int expected = 2;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.24);
+    }
+
+    @Test
+    public void when24to28then2() {
+        Point a = new Point(2, 4);
+        Point b = new Point(2, 8);
+        int expected = 4;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when123to345then3() {
+        Point a = new Point(1, 2, 3);
+        Point b = new Point(3, 4, 5);
+        int expected = 3;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.2);
     }
 }
