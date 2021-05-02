a80df2d2e3dae2ecf2cd4d376414e93ecca3870d
YlaVP <git@github.com:YlaVP/restart.git>
2021-05-02 21:15:02 +0900

    0.1. java.lang.NullPointerException [#341330]

--- out/production/job4j_elementary/ru/job4j/StudentInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Yulia Popova");
-        System.out.println("20.02.2020");
-    }
-}
