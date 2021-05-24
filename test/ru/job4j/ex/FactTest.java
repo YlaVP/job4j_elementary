f324225e497e9e92718a24b19868dae44653f6cf
YlaVP <git@github.com:YlaVP/restart.git>
2021-05-24 22:48:30 +0900

    0.3. Тестирование исключений с junit [#219366]

--- test/ru/job4j/ex/FactTest.java
+++ test/ru/job4j/ex/FactTest.java
@@ -2,6 +2,7 @@ package ru.job4j.ex;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static ru.job4j.ex.Fact.calc;
 
@@ -9,8 +10,9 @@ public class FactTest {
 
   @Test(expected = IllegalArgumentException.class)
     public void whenFactLessThenZero() {
+      int rsl = Fact.calc(-1);
+      assertThat(rsl, is(-1));
 
-       Fact.calc(-1);
 
   }
 }
