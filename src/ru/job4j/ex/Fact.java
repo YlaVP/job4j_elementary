b02517578bf407ab4950f681a768976ab003b1e0
YlaVP <git@github.com:YlaVP/restart.git>
2021-05-24 22:43:59 +0900

    0.2. Кидаем исключение - throw new RuntimeException [#341331]

--- src/ru/job4j/ex/Fact.java
+++ src/ru/job4j/ex/Fact.java
@@ -2,18 +2,24 @@ package ru.job4j.ex;
 
 public class Fact {
     public static void main(String[] args) {
-        int n = -1;
+     int n = -1;
+
+        System.out.println("fact" + calc(n));
+
+    }
+
+
+    public static int calc(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("the factorial cannot be a negative number.");
         }
-
-    }
-
-    public int calc(int n) {
-        int rsl = 1;
-        for (int index = 1; index <= n; index++) {
-            rsl *= index;
+            int rsl = 1;
+            for (int index = 1; index <= n; index++) {
+                rsl *= index;
+            }
+            return rsl;
         }
-        return rsl;
     }
-}
+
+
+
---
+++ test/ru/job4j/ex/FactTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static ru.job4j.ex.Fact.calc;
+
+public class FactTest {
+
+  @Test(expected = IllegalArgumentException.class)
+    public void whenFactLessThenZero() {
+
+       Fact.calc(-1);
+
+  }
+}
