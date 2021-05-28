8f75cccab4cd344345064280f9475c67f3aace84
YlaVP <git@github.com:YlaVP/restart.git>
2021-05-28 21:46:54 +0900

    0.4. Пользовательские исключения. [#219367 #160938]

--- src/ru/job4j/ex/FindEl.java
+++ src/ru/job4j/ex/FindEl.java
@@ -4,23 +4,25 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         /* цикл fori, поскольку нам надо найти индекс искомого элемента в массиве */
-        for (int index = 0; index < value.length; index++) {
-            if (value[index] == key)
-                return index;
+        for (int i = 0; i < value.length; i++) {
+            String element = value[i];
+            if (element.equals(key)) {
+                rsl = i;
+                break;
+            }
         }
-
+        if (rsl == -1) {
+            throw new ElementNotFoundException("there is no such element");
+        }
         return rsl;
     }
 
     public static void main(String[] args) {
         String[] arrayString = new String[]{"1", "2", "3", "4"};
-
         try {
             FindEl.indexOf(arrayString, "5");
-            System.out.println("there is no such element");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
-
         }
     }
 }
