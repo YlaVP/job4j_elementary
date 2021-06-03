6bfdf2acb16de64f85b455fdf279aef61f3b3e63
YlaVP <git@github.com:YlaVP/restart.git>
2021-06-03 21:55:28 +0900

    0. Коллекции, ArrayList, List, Обобщения [#10094]

--- src/ru/job4j/collection/UsageArrayList.java
+++ src/ru/job4j/collection/UsageArrayList.java
@@ -1,16 +1,16 @@
-package collection;
+package ru.job4j.collection;
 
 import java.util.ArrayList;
 
 public class UsageArrayList {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>();
+        ArrayList <String> names = new ArrayList<>();
 
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
 
-        for (Object value : names) {
+        for (String value : names) {
             System.out.println(value);
         }
 
