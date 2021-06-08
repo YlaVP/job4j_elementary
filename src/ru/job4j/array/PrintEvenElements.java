8c1236257794d7bbafc601ac57d77ca7f96bc4da
YlaVP <git@github.com:YlaVP/restart.git>
2021-06-08 21:55:49 +0900

    6.4.1.1. Обход массива с последнего элемента [#361619]

--- src/ru/job4j/array/PrintEvenElements.java
+++ src/ru/job4j/array/PrintEvenElements.java
@@ -5,8 +5,6 @@ public class PrintEvenElements {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         for (int i = 0; i < numbers.length; i++) {
             if (( numbers.length - 1 - i ) % 2 == 0) {
-
-
                 System.out.println("Текущий элемент массива начиная с последнего с четными индексами: "
                         + numbers[numbers.length - 1 - i]);
 
