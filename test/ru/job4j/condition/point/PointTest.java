096059170c0d9b8791bcc9c4d94e644e6bcc1f37
YlaVP <git@github.com:YlaVP/restart.git>
2021-06-12 00:02:24 +0900

    4. Расстояние между точками в трехмерном пространстве. [#122643 #82179]

--- test/ru/job4j/condition/point/PointTest.java
+++ test/ru/job4j/condition/point/PointTest.java
@@ -13,36 +13,45 @@ public class PointTest {
     public void when00to20then2() {
         Point a = new Point(0, 0);
         Point b = new Point(2, 0);
-        int expected = 2;
+        double expected = 2;
         double out = a.distance(b);
 
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, out, 0.1);
     }
 
     @Test
-    public void when12to20then2() {
+    public void when12to20then224() {
         Point a = new Point(1, 2);
         Point b = new Point(2, 0);
-        int expected = 2;
-        double out = a.distance(b);
-        Assert.assertEquals(expected, out, 0.24);
-    }
-
-    @Test
-    public void when24to28then2() {
-        Point a = new Point(2, 4);
-        Point b = new Point(2, 8);
-        int expected = 4;
+        double expected = 2.24;
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void when123to345then3() {
+    public void when24to28then4() {
+        Point a = new Point(2, 4);
+        Point b = new Point(2, 8);
+        double expected = 4;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.1);
+    }
+
+    @Test
+    public void when123to345then283() {
         Point a = new Point(1, 2, 3);
         Point b = new Point(3, 4, 5);
-        int expected = 3;
-        double out = a.distance(b);
-        Assert.assertEquals(expected, out, 0.2);
+        double expected = 2.83;
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when000To111Then173() {
+        double expected = 1.73;
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(1, 1, 1);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
     }
 }
