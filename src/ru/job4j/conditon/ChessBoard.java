22087ee55620671ed659212011d05f53ad4600b5
YlaVP <git@github.com:YlaVP/restart.git>
2021-06-13 11:51:38 +0900

    4.3.6. Math.abs [#373616]

--- src/ru/job4j/conditon/ChessBoard.java
+++ src/ru/job4j/conditon/ChessBoard.java
@@ -2,9 +2,10 @@ package ru.job4j.conditon;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
+        int rsl = 0;
         if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
-            return Math.abs(x2 - x1);
+            rsl = Math.abs(x2 - x1);
         }
-        return 0;
+        return rsl;
     }
 }
